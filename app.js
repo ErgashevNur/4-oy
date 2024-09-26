3657f88f8b48b0a1b84643cdd1d0e823995ed3b5
MuhammadNurulloh <dodgechalanger9@gmail.com>
2024-09-26 22:39:04 +0300

    JavaScript

---
+++ app.js
@@ -0,0 +1,247 @@
+// Boolean 1
+/* let son = prompt("Son kiriting");
+
+if (son >= 0) {
+  console.log("Bu son rost");
+} else if (son < 0) {
+  console.log("Bu son yolg'on");
+} */
+
+// Boolean 2
+/*let a = prompt("Son kiriting");
+
+if (a % 2 == 1) {
+  console.log("Bu son toq");
+} else if (a) {
+  console.log("Bu son juft");
+}*/
+
+// Boolean 3
+/*let a = prompt("Son kiriting");
+
+if (a % 2 == 1) {
+  console.log("Bu son toq");
+} else if (a) {
+  console.log("Bu son juft");
+}*/
+
+// Boolean 4
+/*let a = prompt("Son kiriting");
+let b = prompt("Ikkinchi sonni kiriting");
+
+if (a > 2 && b <= 3) {
+  console.log("Kiritgan raqamingiz to'g'ri");
+} else if (a && b) {
+  console.log("Kiritgan raqamlaringiz noto'g'ri");
+}*/
+
+// Boolean 5
+/*let a = prompt("Birinchi sonni kiriting");
+let b = prompt("Ikkinchi sonni kiriting");
+
+if (a >= 0 && b < -2) {
+  console.log("Kiritgan raqamingiz to'g'ri");
+} else if (a && b) {
+  console.log("Kiritgan raqamlaringiz noto'g'ri");
+}*/
+
+// Boolean 6
+/*let a = prompt("Birinchi sonni kiriting");
+let b = prompt("Ikkinchi sonni kiriting");
+let c = prompt("Uchinchi sonni kiriting");
+
+if (a <= b && b <= c) {
+  console.log("Kiritgan raqamingiz to'g'ri");
+} else if ((a, b, c)) {
+  console.log("Kiritgan raqamlaringiz noto'g'ri");
+}*/
+
+// Boolean 7
+/*let a = prompt("Birinchi sonni kiriting");
+let b = prompt("Ikkinchi sonni kiriting");
+let c = prompt("Uchinchi sonni kiriting");
+
+if (a >= b >= c || c >= b >= a) {
+  console.log("Bu son rost");
+} else {
+  console.log("Bu son yolg'on");
+}*/
+
+// Boolean 8
+/*let a = prompt("Birinchi sonni kiritng");
+let b = prompt("Ikkinchi sonni kiritng");
+
+if (a < b) {
+  console.log("Ikkinchi kiritgan ", b, "soningiz kotta");
+} else if (a > b) {
+  console.log("Birinchi kiritgan", a, " soningiz kotta");
+} else if ((a = b)) {
+  console.log("Bu kiritilgan ikkala son ham teng.");
+}*/
+
+// Boolean 9
+/*let a = prompt("Birinchi sonni kiriting");
+let b = prompt("Ikkinchi sonni kiritng");
+
+if (a % 2 == 1 || b % 2 == 1) {
+  console.log("Kiritgan sonlaringiz ichida toq son mavjud");
+} else {
+  console.log("Kiritgan sonlariz faqat juft sondan iborat ");
+}*/
+
+// Boolean 10
+/*let a = prompt("Birinchi sonni kiriting");
+let b = prompt("Ikkinchi sonni kiritng");
+
+if (a % 2 == 1 && b % 2 == 0) {
+  console.log("Birinchi kiritgan soningiz toq son");
+} else if (b % 2 == 1 && a % 2 == 0) {
+  console.log("Ikkinchi kiritgan soningiz toq son");
+} else {
+  console.log("Kiritgan ikkala soningiz ham juft");
+}*/
+
+// Boolean 11
+/*let a = prompt("Birinchi sonni kiriting");
+let b = prompt("Ikkinchi sonni kiritng");
+
+if (a % 2 == 1 && b % 2 == 1) {
+  console.log("Kiritgan soningiz toq son");
+} else if (b % 2 == 0 && a % 2 == 0) {
+  console.log("Kiritgan soningiz juft son");
+} else {
+  console.log("Kiritgan sonlaringiz orasida juft va toq sonlari aralashgan");
+}*/
+
+// Boolean 12
+/*let a = prompt("Birinchi raqamni kiriting");
+let b = prompt("Ikkinchi raqamni kiriting");
+let c = prompt("Uchinchi raqamni kiriting");
+
+if (a % 2 == 1) {
+  console.log("Birinchi raqam toq");
+} else {
+  console.log("Birinchi raqam juft");
+}
+if (b % 2 == 1) {
+  console.log("Ikkinchi raqam toq");
+} else {
+  console.log("Ikkinchi raqam juft");
+}
+if (c % 2 == 1) {
+  console.log("Uchinchi raqam toq");
+} else {
+  console.log("Uchinchi raqam juft");
+}*/
+
+// Boolean 13
+/*let a = prompt("Birinchi raqamni kiriting");
+let b = prompt("Ikkinchi raqamni kiriting");
+let c = prompt("Uchinchi raqamni kiriting");
+
+if (a % 2 == 1) {
+  console.log("Birinchi raqam toq");
+} else {
+  console.log("Birinchi raqam juft");
+}
+if (b % 2 == 1) {
+  console.log("Ikkinchi raqam toq");
+} else {
+  console.log("Ikkinchi raqam juft");
+}
+if (c % 2 == 1) {
+  console.log("Uchinchi raqam toq");
+} else {
+  console.log("Uchinchi raqam juft");
+}*/
+
+// Boolean 14
+/*let a = prompt("Birinchi raqamni kiriting");
+let b = prompt("Ikkinchi raqamni kiriting");
+let c = prompt("Uchinchi raqamni kiriting");
+
+if (a % 2 == 1) {
+  console.log("Birinchi raqam toq");
+} else {
+  console.log("Birinchi raqam juft");
+}
+if (b % 2 == 1) {
+  console.log("Ikkinchi raqam toq");
+} else {
+  console.log("Ikkinchi raqam juft");
+}
+if (c % 2 == 1) {
+  console.log("Uchinchi raqam toq");
+} else {
+  console.log("Uchinchi raqam juft");
+
+}*/ // Boolean 15
+/*let a = prompt("Birinchi raqamni kiriting");
+let b = prompt("Ikkinchi raqamni kiriting");
+let c = prompt("Uchinchi raqamni kiriting");
+
+if (a % 2 == 1) {
+  console.log("Birinchi raqam toq");
+} else {
+  console.log("Birinchi raqam juft");
+}
+if (b % 2 == 1) {
+  console.log("Ikkinchi raqam toq");
+} else {
+  console.log("Ikkinchi raqam juft");
+}
+if (c % 2 == 1) {
+  console.log("Uchinchi raqam toq");
+} else {
+  console.log("Uchinchi raqam juft");
+}*/
+
+// If 1
+/*let a = prompt("Son kiriting");
+a = parseFloat(a);
+
+if (a >= 0) {
+  console.log(a + 1);
+} else {
+  console.log(a);
+}*/
+
+// If 2
+/*let a = prompt("Son kiriting");
+a = parseFloat(a);
+
+if (a >= 0) {
+  console.log(a - 2);
+} else {
+  console.log(a + 1);
+}*/
+
+// If 3
+/*let a = prompt("Son kiriting");
+a = parseFloat(a);
+
+if (a > 1) {
+  console.log(a + 1);
+} else if (a <= -1) {
+  console.log(a - 2);
+} else {
+  console.log(a + 10);
+}*/
+
+// If 4
+/*let a = parseFloat(prompt("Enter your first num"));
+let b = parseFloat(prompt("Enter your second num"));
+let c = parseFloat(prompt("Enter your third num"));
+let musbatlar = 0;
+
+if (a > 0) {
+  musbatlar++;
+}
+if (b > 0) {
+  musbatlar++;
+}
+if (c > 0) {
+  musbatlar++;
+}
+
+console.log("Musbat sonlar: " + musbatlar);*/
